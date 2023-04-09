227758a750c6b241f443f99aea3988c11e07ecf4
Daniel Quesada <88943207+dnielpy@users.noreply.github.com>
2023-04-09 15:04:06 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hola Kayla he cambiado tu codigo :)");
+Console.WriteLine("Hola Kayla he cambiado tu sadasdasdadasdadsdadsad :)");
 // crear un metodo que te permita leer los txt 
