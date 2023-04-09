6c4fb3ab25e835d56d6eca046692141354145f61
dnielpy <dnielquesada@gmail.com>
2023-04-09 16:21:19 -0500

    work please

--- Program.cs
+++ Program.cs
@@ -1,3 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hola Kayla he cambiado tu sadasdasdadasdadsdadsad :)");
-// crear un metodo que te permita leer los txt 
+﻿
